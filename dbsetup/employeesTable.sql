f7466cd38ce8c7e460df52454f32332e90ee7022
Ben <benakehurst@hotmail.com>
2020-12-05 15:32:17 +0000

    Defualt sql command to setup employee table in db

---
+++ dbsetup/employeesTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `organization` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `salary` int NOT NULL,
+  `status` int DEFAULT '0',
+  `is_deleted` int DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
